58ed72a4d73283f886b8156579ac0fa68f698c50
Judgy53 <alexandre.gaubert94@orange.fr>
2024-12-09 10:28:42 +0000

    Ensure container is safe for git

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo Adding Container to Git Safe Directories
-git config --global --add safe.directory ${containerWorkspaceFolder}
+git config --global --add safe.directory $(pwd)
 
 echo Starting PostgreSQL
 sudo service postgresql start
