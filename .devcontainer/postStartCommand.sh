77d09fa078d6c332ed2d4e8feb57e63d5cab1e8b
Judgy53 <alexandre.gaubert94@orange.fr>
2024-12-09 10:44:18 +0000

    Git config: Prevent duplicate entries in safe.directory

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo Adding Container to Git Safe Directories
+git config --global --unset-all safe.directory $(pwd)
 git config --global --add safe.directory $(pwd)
 
 echo Starting PostgreSQL
