fa4f00b056e334fedae5af87abe613124323318e
NathalieMet <nathalie.metreau@hotmail.fr>
2024-12-19 16:40:16 +0000

    add service card component not finished yet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared/button/button.component';
 import { DropdownButtonComponent } from './shared/dropdown-button/dropdown-button.component';
+import { ServiceCardComponent } from './shared/service-card/service-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, DropdownButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, DropdownButtonComponent, ServiceCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
+  date = new Date();
 
   handleClick() {
     alert('Button clicked!');
