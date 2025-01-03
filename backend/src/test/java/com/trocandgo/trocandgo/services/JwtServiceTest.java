3a1b350a5061d51b2d674a9f04eab1139edfee46
MarcP70 <marcpourias@yahoo.fr>
2025-01-03 14:00:10 +0000

    feat(api-JWT token): Remove unused import

--- backend/src/test/java/com/trocandgo/trocandgo/services/JwtServiceTest.java
+++ backend/src/test/java/com/trocandgo/trocandgo/services/JwtServiceTest.java
@@ -5,13 +5,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.Duration;
-import java.time.Instant;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 
