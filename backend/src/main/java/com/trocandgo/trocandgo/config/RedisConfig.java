8ba378e9a2fcddbee8cce45e0d5fa9c435b15e47
MarcP70 <marcpourias@yahoo.fr>
2025-01-10 13:49:58 +0000

    feat(auth): Add Redis config for  configurate database

---
+++ backend/src/main/java/com/trocandgo/trocandgo/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.trocandgo.trocandgo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Utilisation de StringRedisSerializer pour les clés et les valeurs
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(stringSerializer);
+
+        return template;
+    }
+}
