16b28a0006cf5f0134b182a999e29194f3b4fbec
NathalieMet <nathalie.metreau@hotmail.fr>
2025-01-10 15:06:44 +0000

    feat (header-login): Add logout and changes for header if user is loggedin

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,16 @@ export class LoginService {
 
   private apiUrl = 'http://localhost:8080/api/v1/auth';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   login(loginRequest: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.apiUrl}/login`, loginRequest, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     }).pipe(
+      tap(() => {
+        // Met à jour l'état de connexion après un login réussi
+        this.authService.setLoggedIn(true);
+      }),
       catchError(this.handleError)  // Ajout de la gestion des erreurs
     );
   }
--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -3,6 +3,9 @@ import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 import { SignupComponent } from '../../features/signup/signup.component';
 import { LoginComponent } from '../../features/login/login.component';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +14,25 @@ import { LoginComponent } from '../../features/login/login.component';
     RouterLink,
     ButtonComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    CommonModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isLoggedIn = false;
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    // S'abonner à l'état de connexion
+    this.authService.loggedIn$.subscribe((status) => {
+      this.isLoggedIn = status;
+    });
+  }
+
   @Output() clicked = new EventEmitter<void>();
   isPopupVisible = false;
   isLoginPopupVisible = false;
@@ -48,5 +64,14 @@ export class HeaderComponent {
   onSignupSuccess() {
     this.closePopup(); // Ferme la popup d'inscription
     this.openLoginPopup(); // Ouvre la popup de connexion
+  }
+
+  openCreateAd() {
+    this.router.navigate(['/annonce'])
+  }
+
+  logout() {
+    this.authService.logout();  // Appel de la fonction de déconnexion
+    this.router.navigate(['/']);  // Redirection vers la page de connexion
   }
 }
