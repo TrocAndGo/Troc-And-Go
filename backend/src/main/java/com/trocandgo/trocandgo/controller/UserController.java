936fb5b0c973b21563ff8ea540040900c6020b14
MarcP70 <marcpourias@yahoo.fr>
2025-01-10 14:27:36 +0000

    feat(auth): Update response with a map

--- backend/src/main/java/com/trocandgo/trocandgo/controller/UserController.java
+++ backend/src/main/java/com/trocandgo/trocandgo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.trocandgo.trocandgo.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,18 +16,18 @@ public class UserController {
 
     @GetMapping("/profile")
     @PreAuthorize("hasRole('USER')")
-        public ResponseEntity<String> getUserProfile() {
+        public ResponseEntity<Map<String, String>> getUserProfile() {
 
         // Récupérer l'authentification depuis le SecurityContextHolder
         var authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User is not authenticated");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("error", "User is not authenticated"));
         }
 
         // Récupérer le nom d'utilisateur à partir de l'authentification
         String username = authentication.getName();
 
-        return ResponseEntity.ok("User Profile Data for: " + username);
+        return ResponseEntity.ok(Map.of("message", "User Profile Data for: " + username));
     }
 }
