ffc0790d9439b3c36b9502815b228063070fa5ef
Judgy53 <alexandre.gaubert94@orange.fr>
2025-01-15 11:02:00 +0000

    feat(swagger): Add OpenApiConfig

--- backend/src/main/java/com/trocandgo/trocandgo/config/SecurityConfig.java
+++ backend/src/main/java/com/trocandgo/trocandgo/config/SecurityConfig.java
@@ -58,7 +58,6 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth ->
                 auth.requestMatchers(
                         "/api/v1/auth/**",
-                        "/api/v1/public/**",
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
                         "/swagger-ui.html"
