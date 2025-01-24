c73f2248378cf6ef44dce1fc96b0481dd1e27eff
MarcP70 <marcpourias@yahoo.fr>
2025-01-24 15:37:49 +0000

    feat(ssl): Update redis security

--- backend/src/main/java/com/trocandgo/trocandgo/config/RedisConfig.java
+++ backend/src/main/java/com/trocandgo/trocandgo/config/RedisConfig.java
@@ -2,6 +2,8 @@ package com.trocandgo.trocandgo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -9,6 +11,17 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Configuration Redis
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(System.getenv("redis_host")); // Utiliser la variable d'environnement
+        config.setPort(Integer.parseInt(System.getenv("redis_port")));
+        config.setPassword(RedisPassword.of(System.getenv("redis_pw"))); // Mot de passe Redis
+
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
